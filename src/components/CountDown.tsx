05ce0e9f94aa392dadace646e4fc535951eef208
Keven Andrade <hirlandeer7@gmail.com>
2021-03-23 12:18:49 -0100

    day 5

--- src/components/CountDown.tsx
+++ src/components/CountDown.tsx
@@ -1,17 +1,50 @@
+import { useState, useEffect } from 'react';
 import styles from '../styles/components/CountDown.module.css';
 
 export function CountDown(){
+
+    const [time,setTime] = useState(25*60);
+    const [active,setActive] = useState(false);
+
+    const minutes = Math.floor(time/60);
+    const seconds = time % 60;
+
+    const [minuteL, minuteR] = String(minutes).padStart(2, '0').split('');
+    const [secL, secR] = String(seconds).padStart(2, '0').split('');
+
+    function startCount(){
+       setActive(true);
+    }
+
+    useEffect(() =>{
+       /* console.log(active); */
+       if (active && time >0){
+        setTimeout(() =>{
+            setTime( time - 1 );
+        },1000)
+    }
+    }, [active, time])
+
     return (
-        <div className={styles.countDownContainer}>
-            <div>
-                <span>2</span>
-                <span>5</span>
+        <div>
+            <div className={styles.countDownContainer}>
+                <div>
+                    <span>{minuteL}</span>
+                    <span>{minuteR}</span>
+                </div>
+                <span>:</span>
+                <div>
+                    <span>{secL}</span>
+                    <span>{secR}</span>
+                </div>
             </div>
-            <span>:</span>
-            <div>
-                <span>0</span>
-                <span>0</span>
-            </div>
+            <button
+             type="button"
+             className={styles.startCountButton}
+             onClick={startCount}
+            >
+                Iniciar um ciclo
+            </button>
         </div>
     );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,19 @@
 import React from "react";
+import Head from 'next/head';
 import { CompletedChalenge } from "../components/CompletedChalenge";
 import { CountDown } from "../components/CountDown";
 import Experiencebar from "../components/Experiencebar";
 import { Perfil } from "../components/Perfil";
 import styles from '../styles/components/Home.module.css';
 
+
+
 export default function Home() {
   return (
     <div className={styles.container}>
-
+      <Head>
+        <title>Inicio | Take a Break</title>
+      </Head>
       <Experiencebar />
       <section>
         <div>
